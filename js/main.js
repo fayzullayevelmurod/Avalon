a3d0151c9adea57909e116ada648113e9a305c20
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-12-18 23:08:30 +0500

    header in progress

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 // input mask
-$(document).ready(function () {
-    if ($('input[type="tel"]').length) {
-        $('input[type="tel"]').inputmask({"mask": "+7 999 999-99-99"})
-    }
-})
+let phoneInput = document.getElementById('phone');
+let myForm = document.forms.myForm;
+let result = document.getElementById('result');
+
+phoneInput.addEventListener('input', function (e) {
+    let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+
+myForm.addEventListener('submit', function(e) {
+  phoneInput.value = phoneInput.value.replace(/\D/g, '');
+  result.innerText = phoneInput.value; 
+  
+  e.preventDefault();
+});
 // input mask
